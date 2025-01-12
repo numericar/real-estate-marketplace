fa9ab6abce08003033373d868c063518cdc5bea5
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-01-12 11:54:28 +0700

    sign in api

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { errorInstanceHandler } from "../middlewares/error.middleware.js";
-
 import User from "../models/user.model.js";
 
 export async function signUp(req, res, next) {
@@ -22,6 +22,50 @@ export async function signUp(req, res, next) {
             message: "User created successfully",
             data: null,
         });
+    } catch (error) {
+        next(errorInstanceHandler(400, error.message));
+    }
+}
+
+
+export async function signIn(req, res, next) {
+    try {
+        const { email, password } = req.body;
+
+        const validUser = await User.findOne({ email: email });
+        if (!validUser) {
+            next(404, "User not found");
+            return;
+        } 
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) {
+            next(401, "wrong credebtials");
+            return;
+        }
+
+        const JWT_SECRET = process.env.JWT_SECRET;
+        if (!JWT_SECRET) {
+            next(500, "Authentication failed");
+            return;
+        }
+
+        const token = jwt.sign({
+            id: validUser._id
+        }, JWT_SECRET, {
+            expiresIn: "1h"
+        });
+
+        res.cookie("access_token", token, {
+            httpOnly: true
+        });
+
+        res.status(200).json({
+            status: true,
+            message: "Successful",
+            data: null
+        })
+
     } catch (error) {
         next(errorInstanceHandler(400, error.message));
     }
